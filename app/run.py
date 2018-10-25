a7b6a6ba85dda18c1df4d6bb021429590e4ac0bc
Mateusz Kucz <mati.kucz95@gmail.com>
2018-10-25 01:23:05 +0000

    chore: update build requirements

--- app/run.py
+++ app/run.py
@@ -11,7 +11,6 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
-
 from sklearn.base import BaseEstimator, TransformerMixin
 
 app = Flask(__name__)
