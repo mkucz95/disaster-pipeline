0cd456e39d0b95b10b81743bdd4c0bdf40210966
Mateusz Kucz <mati.kucz95@gmail.com>
2018-10-29 20:31:55 -0700

    bugs: commented out main runline

--- app/run.py
+++ app/run.py
@@ -216,8 +216,8 @@ def go():
         classification_result=classification_results
     )
 
-def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+#def main():
+ #   app.run(host='0.0.0.0', port=3001, debug=True)
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
